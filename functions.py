24858fc7cc0180d1c6460d86bb7e2daa9e81821d
hanyiik <yiixhanyiik@gmail.com>
2021-03-29 16:15:58 +0800

    fix some bugs. 2021/03/29 16:15

--- functions.py
+++ functions.py
@@ -334,14 +334,14 @@ def get_config():
             people_num=30
 
             model='ChebNet'
-            k=3
-            kernel_num=32
+            k=2
+            filter_num=32
         )
     :: 用法:
     """
     parser = argparse.ArgumentParser()
     # 跑一个 Attention 用的 rate
-    parser.add_argument('--rate', type=float, default=0.5, choices=[0.4, 0.5])
+    parser.add_argument('--rate', type=float, default=0.5, choices=[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
 
     # 跑两个 Attention 用的 rate_1, rate_2
     parser.add_argument('--rate_1', type=float, default=0.5, choices=[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
