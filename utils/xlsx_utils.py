1e66548a72f0cb2da7811cf00d99850b26dbc128
hanyiik <yiixhanyiik@gmail.com>
2021-05-11 16:50:59 +0800

    nothing.

--- utils/xlsx_utils.py
+++ utils/xlsx_utils.py
@@ -12,7 +12,7 @@ import os
 
 
 # 【更新哪个数据集】
-DATASET_NAME = 'SEED'
+DATASET_NAME = 'MPED'
 
 # 跑的 model 的版本
 EXPERTS = '2 Experts'
@@ -29,16 +29,16 @@ XLS_PATH= f'../res/{DATASET_NAME}/result.xlsx'
 # UPDATE_XLS_PATH = f'C:/Users/HANYIIK/Desktop/hyk_res/{DATASET_NAME}/result.xlsx'
 # UPDATE_XLS_PATH = f'F:/FB-relu-2GNN-run/res/{DATASET_NAME}/result.xlsx'
 # ---------- Mac 环境 ----------
-UPDATE_XLS_PATH = f'/Users/hanyiik/Desktop/liyang_res/{DATASET_NAME}/result.xlsx'
-# UPDATE_XLS_PATH = f'/Users/hanyiik/Desktop/hyk_res/{DATASET_NAME}/result.xlsx'
+# UPDATE_XLS_PATH = f'/Users/hanyiik/Desktop/liyang_res/{DATASET_NAME}/result.xlsx'
+UPDATE_XLS_PATH = f'/Users/hanyiik/Desktop/hyk_res/{DATASET_NAME}/result.xlsx'
 
 # 【用于更新的 state_dict 文件路径】
 # STATE_DICT_PATH = f'C:/Users/HANYIIK/Desktop/liyang_res/{DATASET_NAME}/state_dict/'
 # STATE_DICT_PATH = f'C:/Users/HANYIIK/Desktop/hyk_res/{DATASET_NAME}/state_dict/'
 # STATE_DICT_PATH = f'F:/FB-relu-2GNN-run/res/{DATASET_NAME}/state_dict/'
 # ---------- Mac 环境 ----------
-STATE_DICT_PATH = f'/Users/hanyiik/Desktop/liyang_res/{DATASET_NAME}/state_dict/'
-# STATE_DICT_PATH = f'/Users/hanyiik/Desktop/hyk_res/{DATASET_NAME}/state_dict/'
+# STATE_DICT_PATH = f'/Users/hanyiik/Desktop/liyang_res/{DATASET_NAME}/state_dict/'
+STATE_DICT_PATH = f'/Users/hanyiik/Desktop/hyk_res/{DATASET_NAME}/state_dict/'
 
 
 # 【最终结果的 xls 文件路径】
